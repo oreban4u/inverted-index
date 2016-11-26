8ffe9b3ca00df51db0d52fdfe9fd80b1d1b996be
Oreoluwa Banwo <oreoluwa.banwo@andela.com>
2016-11-26 08:09:16 +0100

    style: update controller to use arrow functions

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,7 @@ app.controller("testangular", function ($scope){
 	
 	$scope.showIndex = false;	
 	
-	$scope.getter = function() {
+	$scope.getter = () => {
 		$scope.array = $scope.index.createIndex($scope.book);
 		$scope.showIndex = true;
 		$scope.showSearch = false;
@@ -10,14 +10,14 @@ app.controller("testangular", function ($scope){
 		//alert('helloo');
 	};
 
-	function numToArray(n) {
+	numToArray = (n) => {
 		var arr = [];
 		for (var i = 0; i < n; i++) {
 			arr.push(i);
 		}
 		return arr;
 	}
-	function titlesList(n) {
+	titlesList = (n) => {
 		let arr = []
 		for (let i = 0; i < n; i++) {
 			arr.push('Book '+ i);
@@ -29,13 +29,13 @@ app.controller("testangular", function ($scope){
 
 	$scope.uploader = () => {	
 		$scope.index = new Index();
-		file = document.getElementById('upload').files[0]
+		let file = document.getElementById('upload').files[0]
 		console.log(file);
 	  let reader = new FileReader();
 		// var file = result.target.files[0];
 		reader.readAsText(file);
 
-		reader.onload = function(event) { 
+		reader.onload = (event) => { 
 		  console.log(event.target.result);
 		  $scope.book = JSON.parse(event.target.result);
 		  $scope.titles = titlesList($scope.book.length);
