37f7516a1359d7358ec01ebea8d7ba5134d08d5b
Oreoluwa Banwo <oreoluwa.banwo@andela.com>
2016-12-09 17:09:41 +0100

    chore: fix last lint issues and refactor

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -3,7 +3,7 @@
 const emptyFile = require('../testfiles/empty.json');
 const wrongKeyBook = require('../testfiles/wrongstructure.json');
 const validFile = require('../testfiles/books.json');
-const wrongFile = require('../testfiles/wrongfile.json')
+const wrongFile = require('../testfiles/wrongfile.json');
 
 const testInvertedIndex = new Index();
 
